37a3517ee6b45ddb02cff81ce67d44976bcfb014
Diegocarrero <diegocar2413@gmail.com>
2023-05-16 12:03:16 -0400

    add empty files

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-const exerciseController = require("./Controllers/exerciseController.js");
-app.use("/exercises", exerciseController);
+// const exerciseController = require("./Controllers/exerciseController.js");
+// app.use("/exercises", exerciseController);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Fitness4All!");
