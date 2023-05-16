4548826e6a190ad1e954c1252d157fda784d782f
Diegocarrero <diegocar2413@gmail.com>
2023-05-16 19:09:46 -0400

    add seed

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+\c exercises_dev;
+
+INSERT INTO exercises (name, image, muscleGroup, targetedMuscles) VALUES
+('Push Up', 'https://www.fitnesseducation.edu.au/wp-content/uploads/2020/10/Pushups.jpg', 'Chest', 'Pectorals, Anterior Deltoids and Triceps'),
+('Inverted Row', 'https://www.trainingbeta.com/wp-content/uploads/2014/11/pullups-1.climbstrong.jpg', 'Back', 'Lats, Traps and Rhomboids'),
+('Bodyweight Squat', 'https://www.shape.com/thmb/DXIpNFBdZgv8jS7Oa8BY7gme50Y=/1500x0/filters:no_upscale():max_bytes(200000):strip_icc()/basic-squat-e59af9e291834334928a67daa01ded58.png', 'Legs', 'Quads, Glutes and Adductors'),
+('Glute Brige', 'https://opt.net.au/wp-content/uploads/hip-bridge.jpg', 'Legs', 'Hamstrings and Glutes'),
+('Pike Push Up', 'https://thesuperhumanfitness.com/wp-content/uploads/2018/02/Pike-push-up.jpg', 'Shoulders', 'Anterior Deltoids, Triceps, Pectorals, Traps'),
+('Bodyweight Calf Raise', 'https://i.pinimg.com/564x/aa/e6/42/aae64297e34eb0cba5dab0442ee70ef6--workout-men-workout-routines.jpg', 'Legs', 'Gastrocnemius'),
+('Incline Push Up', 'https://www.silversneakers.com/wp-content/uploads/2019/08/SSBlog.Exercise_ModifiedPushUp.jpg', 'Chest', 'Pectorals, Anterior Deltoids and Triceps');
