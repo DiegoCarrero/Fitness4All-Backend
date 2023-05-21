262f3d9979363202cf2575e4d1093fda74c3b685
Diegocarrero <diegocar2413@gmail.com>
2023-05-21 14:39:36 -0400

    edit update and create queries

--- queries/exercises.js
+++ queries/exercises.js
@@ -20,10 +20,10 @@ const getAnExercise = async (id) => {
 }
 
 const updateExercise = async (id, exercise) => {
-    const { name, image, muscleGroup, targetedMuscles } = exercise;
+    const { name, image, muscleGroup, targetedMuscles, difficultyLevel, howTo } = exercise;
 
     try {
-        const updatedExercise = await db.one("UPDATE exercises SET name=$1, image=$2, muscleGroup=$3, targetedMuscles=$4 WHERE id=$5 RETURNING *", [name, image, muscleGroup, targetedMuscles, id]);
+        const updatedExercise = await db.one("UPDATE exercises SET name=$1, image=$2, muscleGroup=$3, targetedMuscles=$4 difficultyLevel=$5, howTo=$6 WHERE id=$7 RETURNING *", [name, image, muscleGroup, targetedMuscles, difficultyLevel, howTo, id]);
         return updatedExercise;
     } catch (e) {
         return e;
@@ -31,10 +31,10 @@ const updateExercise = async (id, exercise) => {
 }
 
 const createExercise = async (exerciseToAdd) => {
-    const { name, image, muscleGroup, targetedMuscles } = exerciseToAdd;
+    const { name, image, muscleGroup, targetedMuscles, difficultyLevel, howTo } = exerciseToAdd;
     
     try {
-        const newExercise = await db.one("INSERT INTO exercises (name, image, muscleGroup, targetedMuscles) VALUES ($1, $2, $3, $4) RETURNING *", [name, image, muscleGroup, targetedMuscles])
+        const newExercise = await db.one("INSERT INTO exercises (name, image, muscleGroup, targetedMuscles, difficultyLevel, howTo) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *", [name, image, muscleGroup, targetedMuscles, difficultyLevel, howTo])
         return newExercise;
     } catch (e) {
         return e;
